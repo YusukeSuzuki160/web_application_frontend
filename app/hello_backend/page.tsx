ef671814098ee2e3eac99c0f21f690f2ecf8dad7
Yusuke Suzuki <suzuki-yusuke160@g.ecc.u-tokyo.ac.jp>
2024-04-30 11:02:58 +0900

    inital commit

---
+++ app/hello_backend/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+export default function Page() {
+    const [data, setData] = useState({ message: "Loading..." })
+
+    useEffect(() => {
+        axios.get('/api/hello/backend').then((res) => {
+            setData(res.data)
+        })
+    }, [])
+    return <div>hello {data.message}!</div>
+}
